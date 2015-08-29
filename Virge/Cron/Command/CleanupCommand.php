f6b9d4cd08526f98e8cf086fc379964ab5150e8c
Kramer <kramer@siosphere.com>
2015-08-28 22:27:24 -0700

    Cleanup, allow configuration

--- Virge/Cron/Command/CleanupCommand.php
+++ Virge/Cron/Command/CleanupCommand.php
@@ -3,6 +3,7 @@
 namespace Virge\Cron\Command;
 
 use Virge\Database;
+use Virge\Core\Config;
 
 /**
  * 
@@ -17,8 +18,12 @@ class CleanupCommand extends \Virge\Cli\Component\Command {
      * minutes worth of history
      */
     public function cleanup () {
+        
+        $config = Config::get('cron');
+        $minutes = isset($config['save_time']) ? $config['save_time'] : 15;
+        
         $startDate = new \DateTime();
-        $startDate->modify('-15 minutes');
+        $startDate->modify("-{$minutes} minutes");
         
         $sql = "DELETE FROM `virge_cron_job` WHERE `finished_on` <= ?";
         Database::query($sql, $startDate);
